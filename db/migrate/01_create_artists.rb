24bf114a49fd43bdf864af29d29ab8495c2f740e
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-28 00:41:06 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -2,7 +2,7 @@ class CreateArtists < ActiveRecord::Migration[4.2]
   def change
     create_table :artists do |t|
       t.string :name
-      t.integer
+      t.integer :song_id
     end
   end
 end
