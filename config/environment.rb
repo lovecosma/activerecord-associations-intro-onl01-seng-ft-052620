662dc09b8f92784833fc984ebaf3aab55efc2ee3
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-28 00:48:26 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,47 @@
 require 'bundler/setup'
 require "sinatra/activerecord"
+require_relative '../app/models/artist.rb'
+require_relative '../app/models/genre.rb'
+require_relative '../app/models/song.rb'
+
+
 Bundler.require
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
+
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  song_id INTEGER,
+  genre_id INTEGER
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+sql2 = <<-SQL
+  CREATE TABLE IF NOT EXISTS genres (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  artist_id INTEGER,
+  song_id INTEGER
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql2)
+
+sql3 = <<-SQL
+  CREATE TABLE IF NOT EXISTS songs (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  artist_id INTEGER,
+  genre_id INTEGER
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql3)
